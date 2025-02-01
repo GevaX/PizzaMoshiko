2b95e65d39f3c27341769af1280813c99860d25e
GevaX <gevax1010@gmail.com>
2025-02-01 11:38:46 +0200

    Assignment 14 - Add SQL database

---
+++ SQL Query/SQLQuery1.sql
@@ -0,0 +1,7 @@
+﻿INSERT INTO [dbo].[Users] ([username], [email], [password_hash], [fname], [lname], [gender], [age], [favorite_topping], [crust_preference], [cheese_level], [spicy_level])
+VALUES
+    ('user1', 'user1@example.com', 'hashedpassword1', 'John', 'Doe', 'Male', '19to30', 'Pepperoni', 'thin', 'regular', 'medium'),
+    ('user2', 'user2@example.com', 'hashedpassword2', 'Jane', 'Smith', 'Female', '31to45', 'Mushroom', 'thick', 'extra', 'mild'),
+    ('user3', 'user3@example.com', 'hashedpassword3', 'Mike', 'Johnson', 'Pizza', '13to18', 'Pineapple', 'extra-crispy', 'all-the-cheese', 'spicy'),
+    ('user4', 'user4@example.com', 'hashedpassword4', 'Emily', 'Brown', 'Female', '46to60', 'Olives', 'surprise-me', 'regular', 'fire'),
+    ('user5', 'user5@example.com', 'hashedpassword5', 'Chris', 'White', 'Male', '1to13', 'Sausage', 'thin', 'extra', 'mild');
--- register.aspx
+++ register.aspx
@@ -13,6 +13,8 @@
             <form action="/submit" method="POST">
                 <label for="username">Username:</label>
                 <input type="text" id="username" name="username" placeholder="Enter your username" required />
+                <label for="email">E-mail adress:</label>
+                <input type="email" id="email" name="email" placeholder="Enter your E-mail adress" required />
                 <label for="password">Password:</label>
                 <input type="password" id="password" name="password" placeholder="Enter your password" required />
 
